55796942e824abd4e90d1bf137e37b9b526304d1
helv <helv@helvdeMacBook-Pro.local>
2024-07-26 20:19:15 +0800

    First commit: Finish Todo.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo"
+import Form from "./components/Form"
+import FilterButton from "./components/FilterButton";
 
-function App() {
+export default function App(props) {
+  const taskList = props.tasks.map((task) => (
+    <Todo key={task.id} id={task.id} name={task.name} completed={task.completed}/>
+  )); 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+
+      </ul>
     </div>
   );
 }
 
-export default App;
