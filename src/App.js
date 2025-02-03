1373e8a918ba526d1f083caef8d14938aa2f7d12
helv <1225658928@qq.com>
2025-02-03 21:41:10 +0800

    finish deleting tasks and sycing completed status

--- src/App.js
+++ src/App.js
@@ -9,10 +9,32 @@ export default function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name) {
-    const newTask = {  id: `todo-${nanoid()}`, name, completed: false };
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
     console.log(tasks);
   }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+
+    setTasks(remainingTasks);
+  }
+  
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    console.log(updatedTasks);
+  }  
+  
 
   const taskList = tasks.map((task) => (
     <Todo
@@ -20,11 +42,13 @@ export default function App(props) {
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
 
-  console.log(taskList);
-  //[<Todo id={task.id} name={task.name} completed={task.completed} />]
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -35,7 +59,7 @@ export default function App(props) {
         <FilterButton filterName="Active" />
         <FilterButton filterName="Completed" />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,12 @@ export default function Todo(props) {
     return (
         <li className="todo stack-small">
             <div className="c-cb">
-                <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+                <input
+                    id={props.id}
+                    type="checkbox"
+                    defaultChecked={props.completed}
+                    onChange={() => props.toggleTaskCompleted(props.id)}
+                />
                 <label className="todo-label" htmlFor={props.id}>
                     {props.name}
                 </label>
@@ -13,9 +18,13 @@ export default function Todo(props) {
                 <button type="button" className="btn">
                     Edit <span className="visually-hidden">Eat</span>
                 </button>
-                <button type="button" className="btn btn__danger">
-                    Delete <span className="visually-hidden">Eat</span>
+                <button
+                    type="button"
+                    className="btn btn__danger"
+                    onClick={() => props.deleteTask(props.id)}>
+                    Delete <span className="visually-hidden">{props.name}</span>
                 </button>
+
             </div>
         </li>
     );
